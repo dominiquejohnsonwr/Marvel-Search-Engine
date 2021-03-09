3295c87fa4bd5fd272825fec993a1f8d1769a658
Dominique Johnson <dominiquejohnson@Dominiques-MacBook-Pro-2.local>
2021-03-09 09:54:07 -0800

    successful API call responds with data

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const pubkey = "4f82b7a10e0ace74012143482aebfa7f";
 const pvtkey = "92c3723fcf94b36ea4f8e86b20324f5ece4d1f04";
-const ts = "timestamp"
+const ts = new Date().getTime()
 
-pm.environment.set("ts", ts);
-pm.environment.set("apikey", pubkey);
+// pm.environment.set("ts", ts);
+// pm.environment.set("apikey", pubkey);
 const message = ts+pvtkey+pubkey;
-const a = CryptoJS.MD5(message);
-pm.environment.set("hash", a.toString());
+const hash = CryptoJS.MD5(message);
+// pm.environment.set("hash", a.toString());
 
-//https://gateway.marvel.com/v1/public/characters?ts={{ts}}&name={{name}}&apikey={{apikey}}&hash={{hash}}
+// const URL = 'https://gateway.marvel.com/v1/public/characters'
 
-const URL = 
+const input = document.querySelector('#blank')
+
+const button = document.querySelector('#search')
+
+const heroForm = document.querySelector('#hero-form')
+
+
+heroForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  // console.log("input field works") -> testing form listener
+  findHero(event)
+})
+ 
+button.addEventListener('click', (event) => {
+  // console.log("button works too") -> testing button functionality 
+ 
+})
 
 
 async function findHero(event) {
+  const URL = 'https://gateway.marvel.com/v1/public/characters?ts='+ ts +'&name=' + input.value + '&apikey=' + pubkey + "&hash=" + hash
   try {
-    const url = BASE_URL+input.value
-    const response = await axios.get(url)
-    console.log(response.data.Search)
-    displayMovie(response.data.Search)
+    const response = await axios.get(URL)
+    console.log(response.data)
   } catch (err) {
     console.log(err.message)
   }
